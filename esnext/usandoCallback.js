501437db285c596b77b8d5bf3f89070a793dec5c
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-17 10:01:22 -0300

    ussandoCallback
    este código, uma série de requisições HTTP é feita para buscar dados de turmas diferentes e, em seguida, os nomes dos alunos dessas turmas são concatenados no array "nomes". O código demonstra um exemplo de encadeamento de chamadas de função assíncrona usando callbacks.

--- esnext/usandoCallback.js
+++ esnext/usandoCallback.js
@@ -1,15 +1,20 @@
-// sem promise...
+// Importando o módulo http
 const http = require("http");
 
+// Definindo uma função chamada getTurma que recebe uma letra e um callback
 const getTurma = (letra, callback) => {
   const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
+
+  // Faz uma requisição HTTP para a URL especificada
   http.get(url, (res) => {
     let resultado = "";
 
+    // Ao receber dados da resposta HTTP, os concatena na variável "resultado"
     res.on("data", (dados) => {
       resultado += dados;
     });
 
+    // Quando a resposta HTTP termina, chama o callback com os dados parseados em formato JSON
     res.on("end", () => {
       callback(JSON.parse(resultado));
     });
@@ -17,14 +22,23 @@ const getTurma = (letra, callback) => {
 };
 
 let nomes = [];
+
+// Chama a função getTurma para as turmas "A", "B" e "C" sequencialmente
 getTurma("A", (alunos) => {
+  // Concatena os nomes dos alunos da turma "A" no array "nomes"
   nomes = nomes.concat(alunos.map((a) => `A: ${a.nome}`));
   console.log(nomes);
+
+  // Chama a função getTurma para a turma "B"
   getTurma("B", (alunos) => {
+    // Concatena os nomes dos alunos da turma "B" no array "nomes"
     nomes = nomes.concat(alunos.map((a) => `B: ${a.nome}`));
     console.log(nomes);
   });
+
+  // Chama a função getTurma para a turma "C"
   getTurma("C", (alunos) => {
+    // Concatena os nomes dos alunos da turma "C" no array "nomes"
     nomes = nomes.concat(alunos.map((a) => `C: ${a.nome}`));
     console.log(nomes);
   });
